bbab4ff0fbf91a511a4d3cf385a36658e076dbcf
tiago <tiago.tdaa@gmail.com>
2017-11-25 23:37:55 +0000

    Changed some files due to the change in the logical model.

--- funcionariosMaisTrabalhadores.sql
+++ funcionariosMaisTrabalhadores.sql
@@ -1,7 +1,7 @@
 USE Oficina;
 
 CREATE VIEW funcionariosMaisTrabalhadores AS
-	SELECT F.Nome, COUNT(*) AS `Nº de Serviços` FROM ServicoFuncionario AS SF
+	SELECT F.Nome, COUNT(*) AS `Nº de Serviços` FROM ServiçoFuncionario AS SF
 		INNER JOIN Funcionario AS F
         ON F.id = SF.idFuncionario
 			GROUP BY F.id 
--- users.sql
+++ users.sql
@@ -1,15 +1,15 @@
-Use Oficina;
+Use oficina;
 
 CREATE USER 'admin@email';
 SET PASSWORD FOR 'admin@email' = PASSWORD('adminportaberta');
 
-GRANT ALL ON Oficina.* TO 'admin@email';
+GRANT ALL ON oficina.* TO 'admin@email';
 
 
 CREATE USER 'funcionario@email';
 SET PASSWORD FOR 'funcionario@email' = PASSWORD ('funcportaberta');
 
-GRANT INSERT ON Servico TO 'funcionario@email';
-GRANT INSERT ON ServicoFunconario TO 'funcionario@email';
+GRANT INSERT ON Serviço TO 'funcionario@email';
+GRANT INSERT ON ServiçoFuncionario TO 'funcionario@email';
 GRANT INSERT ON Funcionario TO 'funcionario@email';
 
